420c971855877861b1eb530afaa7db849cb9f610
Kelsey Strachan <kelseymstrachan@gmail.com>
2024-11-22 14:23:23 -0800

    Add files via upload
    Uploading horse_stats.py module

---
+++ horse_race_simulator/race_data/horse_stats.py
@@ -0,0 +1,13 @@
+# Horse module to create hore, update horse stats and get horse info
+class horse_stats: # In progress
+    def __init__(self, name, age, speed, weight, favoured):
+        self.name = name
+        self.age = age
+        self.speed = speed
+        self.weight = weight
+        self.favoured = favoured
+        
+    def create_horse(self): #In progress
+    
+    def get_horse_info(self): #In progress
+        print(f'Horse name = {self.name}')
