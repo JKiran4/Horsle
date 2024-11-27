cba34d3367e5c4c1126945799605e8d59bc32b60
Kelsey Strachan <kelseymstrachan@gmail.com>
2024-11-27 09:11:10 -0800

    Updated file horse_stats.py

--- horse_race_simulator/race_data/horse_stats.py
+++ horse_race_simulator/race_data/horse_stats.py
@@ -13,51 +13,51 @@ class Horse:
         self.horse_rating = horse_rating
         self.jockey_id = jockey_id
         self.speed = self.update_horse_stats()
-    
+
     def create_horse(csv_filename):
         horse_df = pd.read_csv(csv_filename)
-        
+
         horses = []
-        
+
         # Update requested by group member to incorportate number of horses instead of hard-coding
         num_horses = Race.num_horses
-        
+
         while len(horses) < num_horses: # instead of 10 > use an attribute defined in Race Class
             horse_selection = horse_df.sample(n=1).iloc[0]
-            
+
             horse_id = horse_selection['horse_id']
             horse_age = horse_selection['horse_age']
             actual_weight = horse_selection['actual_weight']
             horse_type = horse_selection['horse_type']
             horse_rating = horse_selection['horse_rating']
             jockey_id = horse_selection['jockey_id']
-            
+
             horse_object = Horse(horse_id, horse_age, actual_weight, horse_type, horse_rating, jockey_id)
-        
+
             if horse_object not in horses:
                 horses.append(horse_object)
         return horses
-    
+
     # Will incorporate randomization to ensure that values will be different every time?
     def update_horse_stats(self):
         random_speed = random.uniform(30.0, 50.0)
         if self.horse_rating > 50:
-            random_speed += 5  
+            random_speed += 5
         if self.horse_age < 3:
-            random_speed -= 2 
+            random_speed -= 2
         elif self.horse_age > 5:
             random_speed -= 5
         elif self.actual_weight < 125:
             random_speed += 5
         return round(random_speed, 2)
-              
+
     def get_horse_info(self):
         print(f'Horse ID: {self.horse_id}, Age: {self.horse_age} years, Weight: {self.actual_weight} lbs, Type: {self.horse_type}, Rating: {self.horse_rating}, Speed: {self.speed}')
 
-        
+
 # Code Used to test
-horses = load_horses('runs.csv')
-    
+horses = create_horse('runs.csv')
+
 # Print information for each horse
 for horse in horses:
     horse.get_horse_info()
