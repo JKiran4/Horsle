1108793001c775a81959fcca3c7c76955ea65acc
Kiran John <john.kiran04@gmail.com>
2024-12-02 22:07:10 -0800

    testing file

--- test_program.py
+++ test_program.py
@@ -1,12 +1,10 @@
-# test_program.py
 
-from horse_race_simulator.simulation.race_simulator import RaceSimulator
-from horse_race_simulator.race_data.race_details import Race
+from horse_race_simulator.simulation.betting import User  # Import the User class from betting
 
-def main():
-    race = Race()
-    simulator = RaceSimulator(race, race.track)
-    simulator.start_race()
+# Create a user instance (starting balance of $1000)
+user = User(start_balance=1000)
 
-if __name__ == "__main__":
-    main()
+# Simulate the race welcome process where the user interacts
+# Here, we're calling `race_welcome` method which runs the game logic
+# In this case, we're assuming the user will select "start" to run the game/
+user.race_welcome()  # Pass the list of horses from the race
