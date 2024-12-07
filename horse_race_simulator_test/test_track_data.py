7be16c4472708b361b2c426576c4fa0d105a5265
Kiran John <john.kiran04@gmail.com>
2024-12-07 13:25:28 -0800

    Add files via upload

--- horse_race_simulator_test/test_track_data.py
+++ horse_race_simulator_test/test_track_data.py
@@ -2,7 +2,7 @@
 
 import unittest
 from random import seed
-from track_data import Track
+from horse_race_simulator.race_data.track_data import Track
 
 # Assuming the Track class is already defined
 class TestTrack(unittest.TestCase):
@@ -30,15 +30,21 @@ class TestTrack(unittest.TestCase):
         print("Running test_create_track")
         self.track.create_track()
         self.assertIsNotNone(self.track.track_venue) # check if venue exists
+        venue_names = ["Pony Speedway", "Canter Canyon", "Saddle Summit", "Dusty Lanes", "Gallop Galley", "Riders Run"]
+        self.assertIn(self.track.track_venue[0], venue_names) # check if venue name is in list
         self.assertGreater(self.track.track_venue[1], 0) # check if the distance is greater than 0
+        self.assertLessEqual(self.track.track_venue[1], 2400)  # Check if the distance is less than or equal to 2400
 
     def test_weather_factor(self):
         print("Running test_weather_factor")
         self.track.weather_factor()
         self.assertIsNotNone(self.track.track_weather) # check if weather exists
+        weather_conditions = ["Sunny", "Overcast", "Rainy", "Snowy"]
+        self.assertIn(self.track.track_weather[0], weather_conditions) # check if weather is in list
         self.assertIsInstance(self.track.track_weather[1], int) # check if the factor associated with weather is an integer
+        self.assertLessEqual(self.track.track_weather[1], 0) # check that the factor of weather is less than or equal to 0
 
-    def test_get_track_info(self):
+    def test_get_track_info(self): # extra test, just making sure no exception raised when printing info
         print("Running test_get_track_info")
         self.track.create_track() # simply check methods are working here
         self.track.weather_factor()
