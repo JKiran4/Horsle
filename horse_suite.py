7622d3bf8afebe0f1be74202b8f75ed9deee1f39
Kiran John <john.kiran04@gmail.com>
2024-12-09 13:19:03 -0800

    Update horse_suite.py
    add tests for track and simulator

--- horse_suite.py
+++ horse_suite.py
@@ -1,5 +1,7 @@
 from horse_race_simulator_test.test_horse_stats import TestHorse
 from horse_race_simulator_test.test_betting import TestUser
+from horse_race_simulator_test.test_track_data import TestTrack
+from horse_race_simulator_test.test_race_simulator import TestRaceSimulation
 
 def horse_suite(): # temporary name, please feel free to change if you have a preference
     suite = unittest.TestSuite()
@@ -8,6 +10,12 @@ def horse_suite(): # temporary name, please feel free to change if you have a pr
     suite.addTest(TestHorse('test_update_horse_stats'))
     suite.addTest(TestUser('test_balance_horse_id'))
     suite.addTest(TestUser('test_take_bet'))
+    suite.addTest(TestUser('test_create_track'))
+    suite.addTest(TestUser('test_weather_factor'))
+    suite.addTest(TestUser('test_get_track_info'))
+    suite.addTest(TestUser('test_race_setup'))
+    suite.addTest(TestUser('test_get_times'))
+    suite.addTest(TestUser('test_get_winning_horse_id'))
     runner = unittest.TextTestRunner()
     print(runner.run(suite))
 
