6e672cff2fe0a8610ef221af10fdb4b5ade4063c
ajayatil <amali.jayatileke14@gmail.com>
2024-12-09 13:30:19 -0800

    Updating for Race and RaceResults classes

--- horse_suite.py
+++ horse_suite.py
@@ -2,6 +2,8 @@ from horse_race_simulator_test.test_horse_stats import TestHorse
 from horse_race_simulator_test.test_betting import TestUser
 from horse_race_simulator_test.test_track_data import TestTrack
 from horse_race_simulator_test.test_race_simulator import TestRaceSimulation
+from horse_race_simulator_test.test_race_details import TestRace
+from horse_race_simulator_test.test_race_results import TestRaceResult
 
 def horse_suite(): # temporary name, please feel free to change if you have a preference
     suite = unittest.TestSuite()
@@ -16,6 +18,17 @@ def horse_suite(): # temporary name, please feel free to change if you have a pr
     suite.addTest(TestRaceSimulation('test_race_setup'))
     suite.addTest(TestRaceSimulation('test_get_times'))
     suite.addTest(TestRaceSimulation('test_get_winning_horse_id'))
+    suite.addTest(TestRace('test_constructor'))
+    suite.addTest(TestRace('test_set_delayed_date'))
+    suite.addTest(TestRace('test_set_date'))
+    suite.addTest(TestRace('test_get_race_info'))
+    suite.addTest(TestRaceResult('test_constructor'))
+    suite.addTest(TestRaceResult('test_get_horse_position'))
+    suite.addTest(TestRaceResult('test_display_options_exit'))
+    suite.addTest(TestRaceResult('test_get_horse_age'))
+    suite.addTest(TestRaceResult('test_get_horse_type'))
+    suite.addTest(TestRaceResult('test_get_horse_weight'))
+    suite.addTest(TestRaceResult('test_get_horse_jockey'))
     runner = unittest.TextTestRunner()
     print(runner.run(suite))
 
